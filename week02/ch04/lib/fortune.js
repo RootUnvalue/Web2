7e79112a058d4557eb4e8aae5a103f23eca1d817
RootUnvalue <mls1218@naver.com>
2023-09-11 09:30:45 +0900

    update ch04

---
+++ week02/ch04/lib/fortune.js
@@ -0,0 +1,11 @@
+const fortuneCookies = [
+    "Conquer your fears or they conquer you.",
+    "봄이 오면 강물은 흐른다.",
+    "You will have a pleasant surpise.",
+    "쉽게 생각하라."
+]
+
+exports.getFortune = () => {
+    const idx = Math.floor(Math.random()*fortuneCookies.length)
+    return fortuneCookies[idx]
+}
