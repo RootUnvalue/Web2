2af5f214ab86a3bc0fba9e1a90b2bad64b946ca8
RootUnvalue <mls1218@naver.com>
2023-11-25 02:08:50 +0900

    update

---
+++ ch12/00-logging.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const fs = require('fs')
+const { env } = require('process')
+
+const app = express()
+
+switch(app.get('env')) {
+    case 'development':
+        app.use(morgan('dev'))
+        break
+    case 'production':
+        const stream = fs.createWriteStream(__dirname + '/access.log',
+        { flags: 'a' }
+        )
+        app.use(morgan('combined', { stream }))
+        break
+}
+
+app.get('*', (req, res) => {
+    res.send('hello!')
+})
+
+const port = process.env.PORT||3000
+app.listen(port, () => console.log('Express Started In ' + `${app.get('env')} at http://localhost:${port}`))
